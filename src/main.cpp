6d2b478336c2754633fff36267e73dc8cb32be07
msarrebressolles <maelsrbs@gmail.com>
2023-12-15 13:11:10 +0000

    répétition programme

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,18 @@ int main() {
   int nbrePlaces = 10;
   // variable permettant de récupérer le choix de l'utilisateur
   int choix;
-  cout << "************ Evaluation 2 ************" << endl << endl;
-  // partie à compléter à partir du 3.1
-  cout << "" << endl <<endl;
-  cout << "1 - Entrée dans le parking" << endl <<endl;
-  cout << "2 - Nombre de places disponibles" << endl <<endl;
-  cout << "3 - Sortie du parking" << endl <<endl;
-  cout << "4 - Quitter" << endl <<endl;
-  cout << "" <<endl <<endl;
-  std::cin >> choix;
-  std::cout << "Vous avez saisi : " << choix << std::endl;
-
+  do {
+    cout << "************ Evaluation 2 ************" << endl << endl;
+    // partie à compléter à partir du 3.1
+    cout << "" << endl << endl;
+    cout << "1 - Entrée dans le parking" << endl << endl;
+    cout << "2 - Nombre de places disponibles" << endl << endl;
+    cout << "3 - Sortie du parking" << endl << endl;
+    cout << "4 - Quitter" << endl << endl;
+    cout << "" << endl << endl;
+    std::cin >> choix;
+    std::cout << "Vous avez saisi : " << choix << std::endl;
+    cout << "" << endl << endl;
+  } while (choix != 4);
   return 0;
 }
